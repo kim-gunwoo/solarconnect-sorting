1afb070563d5bee1168bfc2d5d08203dfba0794a
Park Jein <pjane123@naver.com>
2021-08-17 16:24:43 +0900

    feat/app (#5)
    * add: quickSort
    
    * feat: App.js 내 컴포넌트 추가 및 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
+import { useState, useEffect } from "react";
+import LocaleDateTimer from "components/LocaleDateTimer";
+import { InputField, ResultField } from "components/Field";
+import { quickSort } from "utils/common";
+
 function App() {
-  return <div className="App"></div>;
+  const [input, setInput] = useState("");
+  const [numberArray, setNumberArray] = useState([]);
+  const [asceResult, setAsceResult] = useState("");
+  const [descResult, setDescResult] = useState("");
+
+  const toNumber = (input) => {
+    return Number(input);
+  };
+
+  const filterNaN = (item) => {
+    return !isNaN(item);
+  };
+
+  const startSort = (array) => {
+    const newNumberArray = array.split(",").map(toNumber).filter(filterNaN);
+    setNumberArray(newNumberArray);
+    console.log(newNumberArray);
+    setAsceResult(quickSort(newNumberArray).join(", "));
+  };
+
+  const inputFieldChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  useEffect(() => {
+    if (asceResult) {
+      setTimeout(() => {
+        setDescResult(quickSort(numberArray, true).join(", "));
+      }, 3000);
+    }
+  }, [asceResult, numberArray]);
+
+  return (
+    <div className='App'>
+      <LocaleDateTimer />
+      <InputField fieldName='입력' value={input} onChange={inputFieldChange} />
+      <button onClick={()=>startSort(input)}>sort</button>
+      <ResultField fieldName='오름차순' value={asceResult} />
+      <ResultField fieldName='내림차순' value={descResult} />
+      <LocaleDateTimer locales='en-US' />
+    </div>
+  );
 }
 
 export default App;
