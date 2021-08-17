15ce5f37fcbefd748c77c44d8978d2a3fa1d1a29
git-k <kgunu93@gmail.com>
2021-08-17 17:03:39 +0900

    add : css 추가

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -2,10 +2,23 @@ import styled from "styled-components";
 import InputField from "./InputField";
 import ResultField from "./ResultField";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 20px;
+  background-color: #f2f4f5;
+  text-align: center;
+  font-size: 20px;
+`;
 
-export const FieldHeader = styled.h3``;
+export const FieldHeader = styled.h3`
+  padding-bottom: 20px;
+`;
 
-export const TextArea = styled.textarea``;
+export const TextArea = styled.textarea`
+  width: 100%;
+  height: 100px;
+  border: none;
+  resize: none;
+  overflow: auto;
+`;
 
 export { InputField, ResultField };
