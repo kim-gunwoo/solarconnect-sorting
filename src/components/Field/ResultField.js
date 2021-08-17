cf56482013bc096b1e4c4b555ae2bf82c4a87648
Park <pjane123@naver.com>
2021-08-17 19:35:02 +0900

    fix: ResultField excutionTime 조건부 렌더링

--- src/components/Field/ResultField.js
+++ src/components/Field/ResultField.js
@@ -4,7 +4,7 @@ const ResultField = ({ fieldName, value, excutionTime }) => {
   return (
     <Container>
       <FieldHeader>{fieldName}</FieldHeader>
-      <div>{`${excutionTime.toPrecision(5)}ms`}</div>
+      {excutionTime && <div>{`${excutionTime.toPrecision(3)}ms`}</div>}
       <TextArea value={value} readOnly />
     </Container>
   );
